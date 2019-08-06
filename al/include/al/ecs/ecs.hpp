942155923d290a5d178cb3686acdd738f8a88d99
DebTalen <andrii.storozhov@nure.ua>
2019-08-06 14:04:50 +0300

    add entity validation to ecs

--- al/include/al/ecs/ecs.hpp
+++ al/include/al/ecs/ecs.hpp
@@ -9,48 +9,91 @@ class entity_manager;
 class entity
 {
 public:
+    entity() = delete;
+
+    entity(const entity&) = default;
+
+    entity& operator=(const entity&) = default;
+
+    bool operator==(const entity &e)
+    {
+        return this->m_id == e.m_id;
+    }
+
 	template <typename T>
 	bool has() const
 	{
+        if (! valid())
+        {
+            // TODO: add logging
+            return false;
+        }
 		return m_man.template has<T>(m_id);
 	}
 
 	template <typename T, typename ...Args>
 	bool add(Args&& ...args) const
 	{
+        if (! valid())
+        {
+            // TODO: add logging
+            return false;
+        }
 		return m_man.template add<T, Args...>(m_id, std::forward<Args&&>(args)...);
 	}
 
 	template <typename T>
 	T& get() const
 	{
+        if (! valid())
+        {
+            // TODO: add logging
+        }
 		return m_man.template get<T>(m_id);
 	}
 
 	template <typename T>
 	bool remove() const
 	{
+        if (! valid())
+        {
+            // TODO: add logging
+            return false;
+        }
 		return m_man.template remove<T>(m_id);
 	}
+
+    bool valid() const;
 
 private:
 	friend class ecs;
 
-	entity(eid id, component_manager &m)
-		: m_id(id), m_man(m)
+	entity(eid id, component_manager &m, ecs &e)
+        : m_id(id),
+          m_man(m),
+          m_ecs(e)
 	{
 	}
 
 	eid m_id;
 	component_manager &m_man;
+    ecs &m_ecs;
 };
 
 class ecs
 {
 public:
+    ecs();
+
+    ecs(const ecs&) = delete;
+
+    ecs& operator=(const ecs&) = delete;
+
 	entity create();
 
 	void remove(entity e);
+
+    bool is_valid(entity e) const;
 
 	std::vector<entity> get() const;
 
@@ -64,7 +107,7 @@ public:
 		std::vector<entity> res;
 		for (const auto &e : m_ens)
 		{
-			if (e.has<T>())
+			if (e.has<T>() && is_valid(e))
 				res.push_back(e);
 		}
 		return res;
--- al/src/ecs/ecs.cpp
+++ al/src/ecs/ecs.cpp
@@ -1,10 +1,24 @@
 #include "ecs.hpp"
 
+ecs::ecs()
+{
+    // add the frist entity with invalid the id
+    // so that the condition (entity.m_id < m_ens.size())
+    // is correct for valid entities
+    entity invalid { 0, m_com, *this };
+    m_ens.push_back(invalid);
+}
+
 entity ecs::create()
 {
-	entity e{ m_next_id++, m_com };
+	entity e{ m_next_id++, m_com, *this };
 	m_ens.push_back(e);
 	return e;
+}
+
+bool ecs::is_valid(entity e) const
+{
+    return (e.m_id < m_ens.size()) && (m_ens[e.m_id].m_id != 0);
 }
 
 void ecs::remove(entity e)
@@ -33,4 +47,9 @@ void ecs::add_system(std::unique_ptr<System> ptr)
 void ecs::update()
 {
 	m_sys.update(*this);
+}
+
+bool entity::valid() const
+{
+    return m_ecs.is_valid(*this);
 }
